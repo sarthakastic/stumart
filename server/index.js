3aba184aeb2aaecc7dd097749e2bba9848806d01
sarthakastic <iamsg26@gmail.com>
2023-02-21 02:28:29 +0530

    Initial Setup

--- server/index.js
+++ server/index.js
@@ -1,1 +1,11 @@
-console.log("Hello World");
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("APP IS RUNNING!");
+});
+
+const PORT = 8001;
+
+app.listen(PORT, () => console.log(`Server is running on ${PORT}`));
