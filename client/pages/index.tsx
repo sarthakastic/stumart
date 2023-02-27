b85d0c022ebbc326ad0ae3852c5cb23ebf7134c9
sarthakastic <iamsg26@gmail.com>
2023-02-28 05:13:43 +0530

    Tailwind initialised

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -2,8 +2,6 @@ import Head from "next/head";
 import Typography from "@mui/material/Typography";
 import { Inter } from "@next/font/google";
 
-import Navbar from "../components/Navbar/Navbar";
-
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
@@ -15,7 +13,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Navbar />
+      <div className="bg-red-500">Hello</div>
     </>
   );
 }
